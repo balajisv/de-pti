9e7059a9214fa69af461e21a74524a5ab5b05f5a
std66 <sinkutamas@gmail.com>
2016-06-03 01:00:43 +0200

    Automatikus átirányítás a mobil felületre, ha mobilról nézzük az oldalt.

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+function isMobile() {
+	$Find = array(
+		"android",
+		"iphone",
+		"ipad",
+		"mobile",
+		"windows phone"
+	);
+	
+	$UserAgent = strtolower($_SERVER["HTTP_USER_AGENT"]);
+	
+	foreach ($Find as $MobileUserAgent) {
+		if (strpos($UserAgent, strtolower($MobileUserAgent)) !== false) {
+			return true;
+		}
+	}
+	
+	return false;
+}
 
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+if (isMobile() && count($_GET) == 0) {
+	header("Location: m/index.html");
+}
+else {
+	// change the following paths if necessary
+	$yii=dirname(__FILE__).'/framework/yii.php';
+	$config=dirname(__FILE__).'/protected/config/main.php';
 
-header("Content-Type: text/html; charset=UTF-8");
+	// remove the following lines when in production mode
+	//defined('YII_DEBUG') or define('YII_DEBUG',true);
+	// specify how many levels of call stack should be shown in each log message
+	//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
-Yii::createWebApplication($config)->run();
+	header("Content-Type: text/html; charset=UTF-8");
+
+	require_once($yii);
+	Yii::createWebApplication($config)->run();
+}
