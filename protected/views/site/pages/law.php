ddb099f50b4a87bd9e54e44d033c94cb4ed297c1
std66 <sinkutamas@gmail.com>
2016-09-09 20:16:45 +0200

    A jogi szöveg is Bootstrap-képes

--- protected/views/site/pages/law.php
+++ protected/views/site/pages/law.php
@@ -1,54 +1,59 @@
 <?php
-$this->pageTitle=Yii::app()->name . ' - A DE-PTI nyilatkozatai'
+$this->pageTitle = 'A DE-PTI nyilatkozatai';
 ?>
 
-<h1>A DE-PTI nyilatkozatai</h1>
-
-<h2>Nyilatkozat a feltöltött fájlokról</h2>
-<p style="text-align: justify;">
-	A DE-PTI (későbbiekben: oldal, webhely) lehetővé teszi, hogy az oldalra regisztrált tagok fájlokat tölthessenek fel.
-	Ezen fájlok nem ellenőrzöttek a webhely tulajdonosa által, felhasználásuk csak és kizárólag a fájlt letöltő személy
-	saját felelősségére történik. Az oldal tulajdonosa ezekért a fájlokért, azok tartalmáért, pontosságáért és
-	helyességéért, továbbá az általuk okozott esetleges károkért nem tud és nem is vállal felelősséget.
-	Szerzői jogvédelem által védett tartalmat kizárólag akkor szabad feltölteni az oldalra, amennyiben a szerző(k)
-	szabadon hozzáférhetővé és másolhatóvá tette/tették azt, minden egyéb esetben tilos ilyen fájlt ezen
-	a webhelyen megosztani. Amennyiben mégis megtörténik, a jogsértést észlelő személy a "Kapcsolat" részben
-	megadott e-mail címen köteles értesíteni a tulajdonost és megindokolni a bejelentést. Ebben az esetben a
-	tulajdonos köteles értesíteni a fájlt feltöltő tagot a bejelentésről a regisztráció során megadott e-mail
-	címen, továbbá az indoklást is csatolnia kell az üzenet szövegében, amennyiben az indoklás erről másképp nem
-	rendelkezik.
-</p>
-
-<h2>Nyilatkozat a felhasználók adatainak kezeléséről</h2>
-<p style="text-align: justify;">
-	A felhasználóknak lehetőségük van az oldalra regisztrálni. A regisztrációs folyamathoz meg kell adni
-	egy felhasználónevet, egy jelszót és egy e-mail címet. Az oldal az e-mail cím érvényességét nem ellenőrzi.
-	Az elfelejtett jelszavak visszaállítására és a regisztráció törlésére a felhasználónak nincs lehetősége.
-	A jelszavak hash algoritmussal kerülnek tárolásra.
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<h2>Hogyan kezeljük a feltöltött fájlokat?</h2>
+			<p class="text-align-justify">
+				A DE-PTI (későbbiekben: oldal, webhely) lehetővé teszi, hogy az oldalra regisztrált tagok fájlokat tölthessenek fel.
+				Ezen fájlok nem ellenőrzöttek a webhely tulajdonosa által, felhasználásuk csak és kizárólag a fájlt letöltő személy
+				saját felelősségére történik. Az oldal tulajdonosa ezekért a fájlokért, azok tartalmáért, pontosságáért és
+				helyességéért, továbbá az általuk okozott esetleges károkért nem tud és nem is vállal felelősséget.
+				Szerzői jogvédelem által védett tartalmat kizárólag akkor szabad feltölteni az oldalra, amennyiben a szerző(k)
+				szabadon hozzáférhetővé és másolhatóvá tette/tették azt, minden egyéb esetben tilos ilyen fájlt ezen
+				a webhelyen megosztani. Amennyiben mégis megtörténik, a jogsértést észlelő személy a
+				<a href="mailto:sinkutamas@gmail.com">sinkutamas@gmail.com</a>
+				e-mail címen köteles értesíteni a tulajdonost és megindokolni a bejelentést. Ebben az esetben a
+				tulajdonos köteles értesíteni a fájlt feltöltő tagot a bejelentésről a regisztráció során megadott e-mail
+				címen, továbbá az indoklást is csatolnia kell az üzenet szövegében, amennyiben az indoklás erről másképp nem
+				rendelkezik. Bejelentés esetén a bejelentett fájlt minden esetben törli a tulajdonos a webhelyről.
+			</p>
+		</div>
+	</div>
 	
-	A regisztrált tagok bejelölhetik, mely tantárgyakat teljesítették, ezen információk is tárolásra kerülnek
-	a későbbi felhasználás megkönnyítése érdekében. Feltöltött fájl esetében a fájl adatlapján a fájlt
-	feltöltő tag felhasználóneve nyilvánosságra kerül, amit bárki megtekinthet. Az oldal tulajdonosának
-	lehetősége van megtekinteni az összes regisztrált tag felhasználónevét, e-mail címét, regisztrálásának
-	időpontját és a teljesített tantárgyait, ugyanakkor nincs lehetősége a tagok jelszavainak megtekintésére.
-	Az oldal tulajdonosa ezeket az információkat bizalmasan kezeli, harmadik félnek csak indokolt esetben adhatja
-	ki, amiről az érintett tagot a regisztráció során megadott e-mail címen köteles értesíteni, továbbá az indoklást
-	is csatolnia kell az üzenet szövegében, amennyiben az indoklás erről másképp nem rendelkezik.
-</p>
-
-<h2>Nyilatkozat a webhelyen közzétett információkról</h2>
-<p style="text-align: justify;">
-	A webhelyen közzétett hírek a Kezdőlap menüpontban, a Tantárgyak menüpontban feltüntetett tantárgyak és azok adatai,
-	továbbá a Tudnivalók menüpontban közzétett információk kizárólag tájékoztató jellegűek. Ezen információk pontosságáért,
-	helyességéért és a használatából keletkezett esetleges károkért a webhely tulajdonosa nem tud és nem is vállal
-	felelősséget. Ezen információk forrása a Debreceni Egyetemen üzemelő Neptun Egységes Tanulmányi Rendszer, a Debreceni
-	Egyetem szabályzatai, továbbá a Debreceni Egyetem Informatikai Kar Tanulmányi Osztálya és dolgozói által küldött e-mail
-	üzenetek, valamint a Debreceni Egyetem által kiadott, programtervező informatikus BSc. képzésre vonatkozó "fehér füzet".
-	Az ezekben leírtak a hivatalos és irányadó információk és adatok.
-</p>
-
-<h2>Kiegészítő információk</h2>
-<h3>A webhely tulajdonosa</h3>
-<p>
-	A webhely tulajdonosának neve és e-mail címe a Rólunk menüpontban a Kapcsolat fejezetben tekinthető meg.
-</p>
+	<div class="row">
+		<div class="col-xs-12 col-sm-6">
+			<h2>A felhasználók adatainak kezeléséről</h2>
+			<p class="text-align-justify">
+				A felhasználóknak lehetőségük van az oldalra regisztrálni. A regisztrációs folyamathoz meg kell adni
+				egy felhasználónevet, egy jelszót és egy e-mail címet. Az oldal az e-mail cím érvényességét nem ellenőrzi.
+				Az elfelejtett jelszavak visszaállítására és a regisztráció törlésére a felhasználónak nincs lehetősége.
+				A jelszavak hash algoritmussal kerülnek tárolásra.
+				
+				A regisztrált tagok bejelölhetik, mely tantárgyakat teljesítették, ezen információk is tárolásra kerülnek
+				a későbbi felhasználás megkönnyítése érdekében. Feltöltött fájl esetében a fájl adatlapján a fájlt
+				feltöltő tag felhasználóneve nyilvánosságra kerül, amit bárki megtekinthet. Az oldal tulajdonosának
+				lehetősége van megtekinteni az összes regisztrált tag felhasználónevét, e-mail címét, regisztrálásának
+				időpontját és a teljesített tantárgyait, ugyanakkor nincs lehetősége a tagok jelszavainak megtekintésére.
+				Az oldal tulajdonosa ezeket az információkat bizalmasan kezeli, harmadik félnek csak indokolt esetben adhatja
+				ki, amiről az érintett tagot a regisztráció során megadott e-mail címen köteles értesíteni, továbbá az indoklást
+				is csatolnia kell az üzenet szövegében, amennyiben az indoklás erről másképp nem rendelkezik.
+			</p>
+		</div>
+	
+		<div class="col-xs-12 col-sm-6">
+			<h2>A webhelyen közzétett információkról</h2>
+			<p class="text-align-justify">
+				A webhelyen közzétett hírek a Kezdőlap menüpontban, a Tantárgyak menüpontban feltüntetett tantárgyak és azok adatai,
+				továbbá a Tudnivalók menüpontban közzétett információk kizárólag tájékoztató jellegűek. Ezen információk pontosságáért,
+				helyességéért és a használatából keletkezett esetleges károkért a webhely tulajdonosa nem tud és nem is vállal
+				felelősséget. Ezen információk forrása a Debreceni Egyetemen üzemelő Neptun Egységes Tanulmányi Rendszer, a Debreceni
+				Egyetem szabályzatai, továbbá a Debreceni Egyetem Informatikai Kar Tanulmányi Osztálya és dolgozói által küldött e-mail
+				üzenetek, valamint a Debreceni Egyetem által kiadott, programtervező informatikus BSc. képzésre vonatkozó "fehér füzet".
+				Az ezekben leírtak a hivatalos és irányadó információk és adatok.
+			</p>
+		</div>
+	</div>
+</div>
