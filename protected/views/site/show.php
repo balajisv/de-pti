19f9a26829dcf42efb34b509a9767d742e755240
std66 <sinkutamas@gmail.com>
2016-09-09 16:50:46 +0200

    Az adott hír megjelenése javítva

--- protected/views/site/show.php
+++ protected/views/site/show.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - ' . $model->title;
+$this->pageTitle = $model->title;
 
 $sh = new StringHelper();
 
@@ -15,11 +15,19 @@ $FacebookUrl = Yii::app()->params['domain'] . Yii::App()->createUrl("site/showNe
 
 ?>
 
-<h1><?php print $model->title; ?></h1>
-
-<div style="text-align: justify; margin-bottom: 20px;">
-	<?php print nl2br($model->contents); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 text-align-justify">
+			<p><?php print nl2br($model->contents); ?></p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-xs-12">
+			<h3>Várjuk a véleményedet!</h3>
+			<div class="text-align-center">
+				<div class="fb-comments" data-width="690" data-href="<?php print $FacebookUrl; ?>" data-numposts="5" data-colorscheme="light"></div>
+			</div>
+		</div>
+	</div>
 </div>
-
-<h3>Várjuk a véleményedet!</h3>
-<div class="fb-comments" data-width="690" data-href="<?php print $FacebookUrl; ?>" data-numposts="5" data-colorscheme="light"></div>
